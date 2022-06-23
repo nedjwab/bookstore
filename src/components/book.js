3a8d57150b87216a640ce827d02aa2335c4522f1
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-06-23 21:00:11 +0100

    Refactor the add and remove functionalities

--- src/components/book.js
+++ src/components/book.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteBook } from '../redux/books/books';
+import { fetchBooks, deleteBook } from '../redux/books/books';
 
 export default function Book() {
   const books = useSelector((state) => state.books);
@@ -10,12 +10,21 @@ export default function Book() {
     dispatch(deleteBook(id));
   };
 
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, []);
+
   if (!books) return <p>No Books Available</p>;
 
   return (
     <>
       {books.map((book) => (
         <div key={book.id}>
+          <p>
+            {' '}
+            {book.category}
+          </p>
+          {' '}
           {book.title}
 
           by
